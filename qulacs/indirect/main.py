cdb1d00d100a948b7ed3cf16bdaf8561611e90d8
toshifumi.anan <toshifumi.anan@uzabase.com>
2022-04-04 13:38:32 +0900

    fix main

--- qulacs/indirect/main.py
+++ qulacs/indirect/main.py
@@ -13,6 +13,8 @@ from qulacs import QuantumState, QuantumCircuit
 from scipy.optimize import minimize, Bounds
 
 def cost(random_list):
+  global iteration
+  iteration+=1
   state = QuantumState(n_qubit)
   circuit = QuantumCircuit(n_qubit)
   if config['gate']['type'] == 'indirect_by_none':
@@ -33,30 +35,33 @@ def cost(random_list):
 def record(x):
   global param_history
   global cost_history
+  global iter_history
   param_history.append(x)
   cost_history.append(cost(x))
+  iter_history.append(iteration)
 
-def output(param_history, cost_history):
+def output(param_history, cost_history, iter_history):
   if config['gate']['type'] == 'direct':
     param_path = "results/%squbit_%s_%s_param.txt" % (n_qubit, config['gate']['type'], config['depth'])
     cost_path = "results/%squbit_%s_%s_cost.txt" % (n_qubit, config['gate']['type'], config['depth'])
+    iter_path = "results/%squbit_%s_%s_iter.txt" % (n_qubit, config['gate']['type'], config['depth'])
   else:
     param_path = "results/%squbit_%s_%s_%s_%s_%s_%s_param.txt" % (n_qubit, config['gate']['type'], config['depth'], config['max_time'], config['gate']['is_r_random'], config['gate']['is_cn_random'], config['gate']['is_bn_random'])
     cost_path = "results/%squbit_%s_%s_%s_%s_%s_%s_cost.txt" % (n_qubit, config['gate']['type'], config['depth'], config['max_time'], config['gate']['is_r_random'], config['gate']['is_cn_random'], config['gate']['is_bn_random'])
+    iter_path = "results/%squbit_%s_%s_%s_%s_%s_%s_iter.txt" % (n_qubit, config['gate']['type'], config['depth'], config['max_time'], config['gate']['is_r_random'], config['gate']['is_cn_random'], config['gate']['is_bn_random'])
 
   print(param_history)
   print(cost_history)
+  print(iter_history)
   np.savetxt(param_path, param_history)
   np.savetxt(cost_path, cost_history)
+  np.savetxt(iter_path, iter_history)
 
 def run():
   init_random_list, bounds = randomize(n_qubit, config)
   if config['gate']['type'] != 'direct':
     constraints = create_time_constraints(config['depth'], len(init_random_list))
-  global param_history
-  global cost_history
-  param_history.append(init_random_list)
-  cost_history.append(cost(init_random_list))
+  record(init_random_list)
   # options = {"disp": True, "maxiter": 50, "gtol": 1e-600}
   if config['gate']['type'] == 'direct':
     opt = minimize(cost, init_random_list,
@@ -68,9 +73,7 @@ def run():
                   constraints=constraints,
                   bounds=bounds,
                   callback=record)
-  print("param_history: %s" % param_history)
-  print("cost_history: %s" % cost_history)
-  output(param_history, cost_history)
+  output(param_history, cost_history, iter_history)
 
 ## init
 n_qubit = 6
@@ -78,6 +81,8 @@ qulacs_hamiltonian = init_hamiltonian()
 config = {}
 param_history = []
 cost_history = []
+iter_history = []
+iteration = 0
 
 if __name__ == '__main__':
   args = sys.argv
