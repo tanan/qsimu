4af90246356b3b55ffc30a7da8bc0d456ac0a2f7
toshifumi.anan <toshifumi.anan@uzabase.com>
2022-09-28 11:22:15 +0900

    add single qubit noise

--- qulacs/apps/common/ansatz/direct.py
+++ qulacs/apps/common/ansatz/direct.py
@@ -18,28 +18,42 @@ class DirectAnsatz(Ansatz):
         circuit = QuantumCircuit(self.nqubit)
         for d in range(self.depth):
             for i in range(self.nqubit):
-                circuit.add_gate(
-                    merge(
-                        RY(i, random_list[2 * i + 2 * self.nqubit * d]),
-                        RZ(i, random_list[2 * i + 1 + 2 * self.nqubit * d]),
-                    )
+                circuit = self.add_parametric_rotation_gate(
+                    circuit,
+                    i,
+                    np.array(
+                        [
+                            random_list[2 * i + 2 * self.nqubit * d],
+                            random_list[2 * i + 1 + 2 * self.nqubit * d],
+                        ]
+                    ),
                 )
-                if self.noise["singlequbit"]["enabled"]:
-                    circuit.add_gate(
-                        DepolarizingNoise(i, self.noise["singlequbit"]["value"])
-                    )
             for i in range(self.nqubit // 2):
                 circuit = self.add_cz_gate(circuit, 2 * i, 2 * i + 1)
             for i in range(self.nqubit // 2 - 1):
                 circuit = self.add_cz_gate(circuit, 2 * i + 1, 2 * i + 2)
         for i in range(self.nqubit):
-            circuit.add_gate(
-                merge(
-                    RY(i, random_list[2 * i + 2 * self.nqubit * self.depth]),
-                    RZ(i, random_list[2 * i + 1 + 2 * self.nqubit * self.depth]),
-                )
+            circuit = self.add_parametric_rotation_gate(
+                circuit,
+                i,
+                np.array(
+                    [
+                        random_list[2 * i + 2 * self.nqubit * self.depth],
+                        random_list[2 * i + 1 + 2 * self.nqubit * self.depth],
+                    ]
+                ),
             )
 
+        return circuit
+
+    def add_parametric_rotation_gate(self, circuit, target_qubit, params):
+        circuit.add_gate(
+            merge(RY(target_qubit, params[0]), RZ(target_qubit, params[1]),)
+        )
+        if self.noise["singlequbit"]["enabled"]:
+            circuit.add_gate(
+                DepolarizingNoise(target_qubit, self.noise["singlequbit"]["value"])
+            )
         return circuit
 
     def add_cz_gate(self, circuit, control_qubit, target_qubit):
