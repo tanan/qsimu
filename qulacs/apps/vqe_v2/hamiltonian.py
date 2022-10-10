233543babcc5bf2031c77e7c10b64c7724146985
tanan <toshifumi.anan@gmail.com>
2022-10-11 01:12:44 +0900

    hami coef be minus

--- qulacs/apps/vqe_v2/hamiltonian.py
+++ qulacs/apps/vqe_v2/hamiltonian.py
@@ -9,9 +9,9 @@ def create_ising_hamiltonian(nqubit):
 def create_qubit_operator(nqubit):
     hami = QubitOperator()
     for i in range(nqubit):
-        hami = hami + QubitOperator("X" + str(i))
+        hami = hami + (-1.0 * QubitOperator("X" + str(i)))
         for j in range(i + 1, nqubit):
             if i + 1 == j:
-                hami = hami + QubitOperator("Z" + str(i) + " " + "Z" + str(j))
+                hami = hami + (-1.0 * QubitOperator("Z" + str(i) + " " + "Z" + str(j)))
 
     return hami
